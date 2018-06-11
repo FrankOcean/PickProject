ce8e513940bd3d896329ad67a6039957bf3af20f
FrankOcean <1358819273@qq.com>
2018-06-11 22:35:45 +0800

    更新内存泄露检测工具

--- PickProject/Module/Base/BaseViewController.swift
+++ PickProject/Module/Base/BaseViewController.swift
@@ -7,38 +7,38 @@
 //
 
 import UIKit
-//import LifetimeTracker
+import LifetimeTracker
 
-//class DetailItem: LifetimeTrackable {
-//    
-//    class var lifetimeConfiguration: LifetimeConfiguration {
-//        return LifetimeConfiguration(maxCount: 3, groupName: "Detail Item", groupMaxCount: 3)
-//    }
-//    
-//    init() {
-//        self.trackLifetime()
-//    }
-//}
+class DetailItem: LifetimeTrackable {
+    
+    class var lifetimeConfiguration: LifetimeConfiguration {
+        return LifetimeConfiguration(maxCount: 3, groupName: "Detail Item", groupMaxCount: 3)
+    }
+    
+    init() {
+        self.trackLifetime()
+    }
+}
 
-class BaseViewController: UIViewController {
+class BaseViewController: UIViewController, LifetimeTrackable {
   
     fileprivate var statusBarShouldLight = false
 
-//    static var lifetimeConfiguration = LifetimeConfiguration(maxCount: 1, groupName: "VC")
-//
-//    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-//        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-//
-//        trackLifetime()
-//
-//    }
+    static var lifetimeConfiguration = LifetimeConfiguration(maxCount: 1, groupName: "VC")
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+        trackLifetime()
+
+    }
     
-//    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//
-//        trackLifetime()
-//    }
-//
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        trackLifetime()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         automaticallyAdjustsScrollViewInsets = false
